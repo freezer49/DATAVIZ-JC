cb5b7f4ee1f193a6fd4867fc6c9d4d307c4fbe40
Chloe Verglas <chloe.verglas@gmail.com>
2025-11-25 09:39:37 +0100

    footer ajout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Image from "./components/Image";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
       <Header />
       <Image />
+      <Footer />
     </div>
   );
 }
