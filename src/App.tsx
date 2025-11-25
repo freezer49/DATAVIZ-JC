aaea69c78e76205da5cd4d99666c3f5c6eb61bfe
JeannePeuron <jeanne.peuron@gmail.com>
2025-11-25 09:43:35 +0100

    LineChart dans App

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import "./App.css";
 import Image from "./components/Image";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import LineChart from "./components/LineChart";
+import { Line } from "recharts";
 
 function App() {
   return (
     <div>
       <Header />
       <Image />
+      <LineChart />
       <Footer />
     </div>
   );
