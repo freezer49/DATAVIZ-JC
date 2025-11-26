edc95ae71bb7e9c511b8121a85418d2478970f87
Chloe Verglas <chloe.verglas@gmail.com>
2025-11-26 09:54:33 +0100

    page analyse

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,18 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
   return (
     <header className="bg-white/80 backdrop-blur-md shadow-lg">
       <nav className="flex gap-8 px-8 py-4">
-        <a
-          href="/"
-          className="text-[#220100] font-medium hover:opacity-70 transition-opacity active:border-black"
-        >
+        <Link to="/" className="text-[#220100] font-medium hover:opacity-70">
           Accueil
-        </a>
-        <a
-          href="/analyse"
-          className="text-[#220100] font-medium hover:opacity-70 transition-opacity active:border-black"
+        </Link>
+        <Link
+          to="/analyse"
+          className="text-[#220100] font-medium hover:opacity-70"
         >
           Analyse
-        </a>
+        </Link>
       </nav>
     </header>
   );
