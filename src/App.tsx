ea9b56799bea283bb478ccda3131a04897eaeb5b
Chloe Verglas <chloe.verglas@gmail.com>
2025-11-26 14:26:27 +0100

    creation de graphique type de tournage

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Image from "./components/Image";
 import LineChart from "./components/LineChart";
-// import ShootingTypeChart from "./components/ShootingTypeChart";
+import ShootingTypeChart from "./components/ShootingTypeChart";
 import TopDirectors from "./components/TopDirectors";
 
 export default function App() {
@@ -19,8 +19,8 @@ export default function App() {
             <div className="p-8">
               <h1 className="text-3xl font-bold text-center mb-8">Analyse</h1>
               <LineChart />
+              <ShootingTypeChart />
               <TopDirectors />
-              {/* <ShootingTypeChart /> */}
             </div>
           }
         />
