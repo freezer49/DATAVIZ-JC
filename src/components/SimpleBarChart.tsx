0160743db608fb46f52c68ba76274888279a67fa
JeannePeuron <jeanne.peuron@gmail.com>
2025-11-27 12:27:05 +0100

    Création BarChart

---
+++ src/components/SimpleBarChart.tsx
@@ -0,0 +1,100 @@
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
+
+async function Fetch() {
+  const response = await fetch(
+    "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/lieux-de-tournage-a-paris/records?select=ardt_lieu&limit=100&offset=0"
+  );
+
+  const data = await response.json();
+
+  return data.results;
+}
+
+// #region Sample data
+const data = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
+// #endregion
+const SimpleBarChart = () => {
+  return (
+    <BarChart
+      layout="vertical"
+      style={{
+        width: "100%",
+        maxWidth: "700px",
+        maxHeight: "70vh",
+        aspectRatio: 1.618,
+      }}
+      data={data}
+      margin={{
+        top: 5,
+        right: 0,
+        left: 0,
+        bottom: 5,
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+
+      <XAxis type="number" />
+      <YAxis type="category" dataKey="name" />
+
+      <Tooltip />
+      <Legend />
+
+      <Bar dataKey="pv" fill="#8884d8" />
+      <Bar dataKey="uv" fill="#82ca9d" />
+    </BarChart>
+  );
+};
+
+export default SimpleBarChart;
